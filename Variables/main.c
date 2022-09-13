9300bad6d97d5d0d53199d5071045aefd06dec76
Marcel Römer <m1smr@hotmail.com>
2022-09-13 19:38:04 +0200

    Variables

---
+++ Variables/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int age = 23;
+	printf("I'm Marcel, %i years old\n", age);
+
+	float cm = 2.54;
+	printf("There are %f cm in once inch\n", cm);
+
+	char c = 'M';
+	printf("My favourite letter is %c", c);
+}
